d696da04ff3cf63f213a522d9c467f2e29451a5d
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-17 23:30:19 -0400

    using useState to toggle buttons to change color class and increment count numbers

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 // App.js is a component which is one file that holds all the js logic, html, styling.
 
-import React from 'react';
+import React, { useState } from 'react';
 import Tweet from './Tweet';
 
 function App() {
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  };
+
   return (
     // jsx
     <div className='app'>
-      <Tweet
+      <h1 className={isRed ? 'red' : ''}>Change my color! </h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count} </h1>
+      {/* <Tweet
         name='Dev Ed'
         message='Check out my courses and become more creative'
         likes='5'
       />
       <Tweet name='Mosh' message='My new course is available' likes='10000' />
       <Tweet name='Phen' message='This is a random tweet' likes='2000' />
-      <Tweet name='Traversy' message='700k my dudes' likes='10000000' />
+      <Tweet name='Traversy' message='700k my dudes' likes='10000000' /> */}
     </div>
   );
 }
