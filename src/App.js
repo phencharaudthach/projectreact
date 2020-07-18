4e6019e735be35ada74026e6522a6fb1096a6641
phencharaudthach <53065868+phencharaudthach@users.noreply.github.com>
2020-07-17 23:38:27 -0400

    useState to render data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 // App.js is a component which is one file that holds all the js logic, html, styling.
 
-import React from 'react';
+import React, { useState } from 'react';
 import Tweet from './Tweet';
 
 function App() {
+  const [users, setUsers] = useState([
+    { name: 'Ed', message: 'Hi there' },
+    { name: 'John', message: 'Hi person' },
+    { name: 'Mosh', message: 'I am awesome' },
+  ]);
+
   return (
-    // jsx
     <div className='app'>
-      <Tweet
-        name='Dev Ed'
-        message='Check out my courses and become more creative'
-        likes='5'
-      />
-      <Tweet name='Mosh' message='My new course is available' likes='10000' />
-      <Tweet name='Phen' message='This is a random tweet' likes='2000' />
-      <Tweet name='Traversy' message='700k my dudes' likes='10000000' />
+      {users.map((user) => (
+        <Tweet name={user.name} message={user.message} />
+      ))}
+
+      {/* jsx
+    <Tweet */}
+      {/* //     name='Dev Ed'
+    //     message='Check out my courses and become more creative'
+    //     likes='5'
+    //   />
+    //   <Tweet name='Mosh' message='My new course is available' likes='10000' />
+    //   <Tweet name='Phen' message='This is a random tweet' likes='2000' />
+    //   <Tweet name='Traversy' message='700k my dudes' likes='10000000' /> */}
     </div>
   );
 }
